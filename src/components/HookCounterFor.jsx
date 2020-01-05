d4f454ccc70533a5aae1843f9575be0aa48133d7
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-01-05 19:15:29 -0300

    useState with array

---
+++ src/components/HookCounterFor.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function HookCounterFor() {
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 10) + 1
+      }
+    ]);
+  };
+
+  return (
+    <div className="counterfor">
+      <button onClick={addItem}>Add a number</button>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
