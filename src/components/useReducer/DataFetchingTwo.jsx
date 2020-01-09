f647c7333753b9227dc5eab150db483b64af3ff7
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-01-09 15:24:30 -0300

    fetching data with useReducer and useEffect

---
+++ src/components/useReducer/DataFetchingTwo.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+const DataFtechingTwo = () => {
+  const initialState = {
+    loading: true,
+    post: {},
+    error: '',
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS':
+        return {
+          loading: false,
+          post: action.payload,
+          error: '',
+        };
+      case 'FETCH_ERROR':
+        return {
+          loading: false,
+          post: {},
+          error: 'Something went wrong',
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(res => {
+        dispatch({ type: 'FETCH_SUCCESS', payload: res.data });
+      })
+      .catch(error => {
+        dispatch({ type: 'FETCH_SUCCESS'})
+      });
+  }, []);
+
+  return (
+    <div>
+      {state.loading ? 'Loading' : state.post.title}
+      {state.error ? state.error : null}
+    </div>
+  );
+};
+
+export default DataFtechingTwo;
