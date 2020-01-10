f42e690c583a844a164faf6056004ba7e2074d15
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-01-10 13:18:48 -0300

    main file and project dependencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './App.css';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterFor from './components/HookCounterFor';
+import HookTimer from './components/useRef/HookTimer';
+
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
+
+// export const CountContext = React.createContext();
+
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case 'increment':
+//       return state + 1;
+//     case 'decrement':
+//       return state - 1;
+//     case 'reset':
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
 
 function App() {
+  // const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="App">
-     <HookCounter />
-     <br />
-     <HookCounterTwo />
-     <br />
-     <HookCounterThree />
-     <br />
-     <HookCounterFor />
-     <br />
-     <br />
+    <div className='App'>
+      <HookTimer />
     </div>
   );
 }
